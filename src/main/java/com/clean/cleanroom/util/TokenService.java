2c4ee311ed3bbaf8f80e2ed6b4493dd5900c7074
sion <topaegr3525@kakao.com>
2024-07-31 14:08:59 +0900

    refactor : 검증로직 어노테이션 수정

--- src/main/java/com/clean/cleanroom/util/TokenService.java
+++ src/main/java/com/clean/cleanroom/util/TokenService.java
@@ -3,7 +3,6 @@ package com.clean.cleanroom.util;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Component;
 
-@Component
 public interface TokenService {
 
     String getEmailFromRequest(HttpServletRequest request);
--- src/main/java/com/clean/cleanroom/util/TokenServiceImpl.java
+++ src/main/java/com/clean/cleanroom/util/TokenServiceImpl.java
@@ -1,7 +1,9 @@
 package com.clean.cleanroom.util;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TokenServiceImpl implements TokenService {
 
     private final JwtUtil jwtUtil;
