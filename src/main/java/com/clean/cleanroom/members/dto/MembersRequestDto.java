4cc3a3ced18959029c2c33607569d3c0e7ab0793
jhchoen <jh.choen@kakao.com>
2024-08-01 10:52:08 +0900

    feat : validation 전화번호 '-' 제거

--- src/main/java/com/clean/cleanroom/members/dto/MembersRequestDto.java
+++ src/main/java/com/clean/cleanroom/members/dto/MembersRequestDto.java
@@ -22,8 +22,8 @@ public class MembersRequestDto {
     private String nick;
 
     @NotEmpty(message = "전화번호는 필수 입력 항목입니다.")
-    @Pattern(regexp = "^01\\d{1}-\\d{4}-\\d{4}$",
-            message = "전화번호는 010-1234-5678 형식이어야 합니다.")
+    @Pattern(regexp = "^01\\d{9}$",
+            message = "전화번호는 01012345678 형식이어야 합니다.")
     private String phoneNumber;
 
 }
