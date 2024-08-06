ff97b295aaeb7fd4902d9e5020ed030d88299f7a
jhchoen <jh.choen@kakao.com>
2024-08-06 17:36:01 +0900

    feat : getProfile 생성 및 에러처리 적용

--- src/main/java/com/clean/cleanroom/members/repository/MembersRepository.java
+++ src/main/java/com/clean/cleanroom/members/repository/MembersRepository.java
@@ -6,4 +6,7 @@ import java.util.Optional;
 
 public interface MembersRepository extends JpaRepository<Members, Long> {
     Optional<Members> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByNick(String nick);
+    boolean existsByPhoneNumber(String phoneNumber);
 }
--- src/main/java/com/clean/cleanroom/members/service/MembersService.java
+++ src/main/java/com/clean/cleanroom/members/service/MembersService.java
@@ -1,10 +1,14 @@
 package com.clean.cleanroom.members.service;
 
+import com.clean.cleanroom.exception.CustomException;
+import com.clean.cleanroom.exception.ErrorMsg;
+import com.clean.cleanroom.members.dto.MembersGetProfileResponseDto;
 import com.clean.cleanroom.members.dto.MembersProfileResponseDto;
 import com.clean.cleanroom.members.dto.MembersRequestDto;
 import com.clean.cleanroom.members.dto.MembersSignupResponseDto;
 import com.clean.cleanroom.members.entity.Members;
 import com.clean.cleanroom.members.repository.MembersRepository;
+import com.clean.cleanroom.util.JwtUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,30 +16,66 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class MembersService {
     private final MembersRepository membersRepository;
+    private final JwtUtil jwtUtil;
 
-    public MembersService(MembersRepository membersRepository) {
+    public MembersService(MembersRepository membersRepository, JwtUtil jwtUtil) {
         this.membersRepository = membersRepository;
+        this.jwtUtil = jwtUtil;
     }
 
     @Transactional
     public MembersSignupResponseDto signup(MembersRequestDto requestDto) {
+        // email 유무
+        if (membersRepository.existsByEmail(requestDto.getEmail())){
+            throw new CustomException(ErrorMsg.DUPLICATE_EMAIL);
+        }
+        // Nick 존재 유무
+        if (membersRepository.existsByNick(requestDto.getNick())){
+            throw new CustomException(ErrorMsg.DUPLICATE_NICK);
+        }
+        // phoneNumber 존재 유무
+        if (membersRepository.existsByPhoneNumber(requestDto.getPhoneNumber())){
+            throw new CustomException(ErrorMsg.DUPLICATE_PHONENUMBER);
+        }
+        // 패스워드 Hashing
         Members members = new Members(requestDto);
-//        패스워드 Hashing
         members.setPassword(requestDto.getPassword());
         membersRepository.save(members);
         return new MembersSignupResponseDto(members);
     }
 
     @Transactional
-    public MembersProfileResponseDto profile(Long id, MembersRequestDto requestDto) {
-        Members members = membersRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("잘못된 ID : " + id));
-//         비밀번호 일치 확인
+    public MembersProfileResponseDto profile(String token, MembersRequestDto requestDto) {
+        String email = jwtUtil.extractEmail(token);
+        // email 유무
+        Members members = membersRepository.findByEmail(email).orElseThrow(
+                () -> new CustomException(ErrorMsg.INVALID_ID));
+        // Nick 존재 유무
+        if (!members.getEmail().equals(requestDto.getEmail()) &&
+                membersRepository.existsByNick(requestDto.getNick())){
+            throw new CustomException(ErrorMsg.DUPLICATE_NICK);
+        }
+        // phoneNumber 존재 유무
+        if (!members.getEmail().equals(requestDto.getEmail()) &&
+                membersRepository.existsByPhoneNumber(requestDto.getPhoneNumber())){
+            throw new CustomException(ErrorMsg.DUPLICATE_PHONENUMBER);
+        }
+        // 비밀번호 일치 확인
         if (!members.checkPassword(requestDto.getPassword())) {
-            throw new IllegalArgumentException("잘못된 비밀번호");
+            throw new CustomException(ErrorMsg.PASSWORD_INCORRECT);
         }
         members.members(requestDto);
         membersRepository.save(members);
         return new MembersProfileResponseDto(members);
+    }
+
+
+    public MembersGetProfileResponseDto getProfile(String token) {
+        // email 존재 유무
+        String email = jwtUtil.extractEmail(token);
+        Members members = membersRepository.findByEmail(email).orElseThrow(
+                () -> new CustomException(ErrorMsg.INVALID_ID)
+        );
+        return new MembersGetProfileResponseDto(members);
     }
 }
