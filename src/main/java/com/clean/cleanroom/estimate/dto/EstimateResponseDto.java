cbf2b0389213ec6d4ebaebef552a2a2515da0900
hajin428 <amm_321@naver.com>
2024-08-08 14:30:30 +0900

    refactor : 견적 승인 API 수정

--- src/main/java/com/clean/cleanroom/estimate/dto/EstimateResponseDto.java
+++ src/main/java/com/clean/cleanroom/estimate/dto/EstimateResponseDto.java
@@ -8,9 +8,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class EstimateResponseDto {
 
+    private boolean approve;
     private String message;
 
     public EstimateResponseDto(Estimate estimate) {
+        this.approve = true;
         this.message = "견적이 승인 되었습니다.";
     }
 }
