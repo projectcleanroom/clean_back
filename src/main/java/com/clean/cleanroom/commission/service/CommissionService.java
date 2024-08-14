bc336f68695ce22a6685391a2b2993bdcce15aa3
hajin428 <amm_321@naver.com>
2024-08-15 05:47:10 +0900

    refactor : commission dto 수정

--- src/main/java/com/clean/cleanroom/commission/service/CommissionService.java
+++ src/main/java/com/clean/cleanroom/commission/service/CommissionService.java
@@ -187,6 +187,7 @@ public class CommissionService {
                 commission.getCleanType(),
                 commission.getDesiredDate(),
                 commission.getSignificant(),
+                commission.getImage(),
                 estimateResponseDtos
         );
     }
