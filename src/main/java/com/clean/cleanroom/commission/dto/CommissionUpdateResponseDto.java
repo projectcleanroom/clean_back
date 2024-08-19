c2c6bd4e5fa0c10cea4848ef3fa14c7a265fb05c
sion <topaegr3525@kakao.com>
2024-08-19 22:29:44 +0900

    refactor : 청소의뢰 수정 API 동작시 Status가 나타나도록 수정

--- src/main/java/com/clean/cleanroom/commission/dto/CommissionUpdateResponseDto.java
+++ src/main/java/com/clean/cleanroom/commission/dto/CommissionUpdateResponseDto.java
@@ -3,6 +3,7 @@ package com.clean.cleanroom.commission.dto;
 import com.clean.cleanroom.commission.entity.Commission;
 import com.clean.cleanroom.enums.CleanType;
 import com.clean.cleanroom.enums.HouseType;
+import com.clean.cleanroom.enums.StatusType;
 import lombok.Getter;
 import java.time.LocalDateTime;
 
@@ -17,6 +18,7 @@ public class CommissionUpdateResponseDto {
     private Long addressId;
     private LocalDateTime desiredDate;
     private String significant;
+    private StatusType statusType;
 
 
 
@@ -29,6 +31,7 @@ public class CommissionUpdateResponseDto {
         this.addressId = commission.getAddress().getId();
         this.desiredDate = commission.getDesiredDate();
         this.significant = commission.getSignificant();
+        this.statusType = commission.getStatus();
     }
 
 }
