e59bac57e21c044f1f137fab171e4be9f7c88ab9
hajin428 <amm_321@naver.com>
2024-08-20 00:07:58 +0900

    test : jwt 테스트 코드 추가

--- src/test/java/com/clean/cleanroom/util/JwtUtilTest.java
+++ src/test/java/com/clean/cleanroom/util/JwtUtilTest.java
@@ -5,14 +5,13 @@ import com.clean.cleanroom.exception.ErrorMsg;
 import com.clean.cleanroom.jwt.entity.RefreshToken;
 import com.clean.cleanroom.jwt.repository.RefreshTokenRepository;
 import com.clean.cleanroom.members.repository.MembersRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.MalformedJwtException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpHeaders;
 
 import java.security.Key;
 import java.util.Optional;
@@ -66,6 +65,61 @@ class JwtUtilTest {
     }
 
     @Test
+    void extractBearerToken_Success() {
+        // Given: 올바른 Bearer 토큰이 주어진 경우
+        String header = "Bearer validToken";
+
+        // When: extractBearerToken 메서드 호출
+        String token = jwtUtil.extractBearerToken(header);
+
+        // Then: Bearer 접두사가 제거된 토큰이 반환되는지 확인
+        assertEquals("validToken", token);
+    }
+
+    @Test
+    void extractBearerToken_MissingHeader_ThrowsException() {
+        // Given: Authorization 헤더가 없는 경우
+        String header = null;
+
+        // When & Then: 예외가 발생하는지 확인
+        CustomException exception = assertThrows(CustomException.class, () -> jwtUtil.extractBearerToken(header));
+        assertEquals(ErrorMsg.MISSING_AUTHORIZATION_HEADER.getCode(), exception.getCode());
+    }
+
+    @Test
+    void extractBearerToken_InvalidHeader_ThrowsException() {
+        // Given: Bearer로 시작하지 않는 헤더
+        String header = "InvalidScheme token";
+
+        // When & Then: 예외가 발생하는지 확인
+        CustomException exception = assertThrows(CustomException.class, () -> jwtUtil.extractBearerToken(header));
+        assertEquals(ErrorMsg.MISSING_AUTHORIZATION_HEADER.getCode(), exception.getCode());
+    }
+
+    @Test
+    void parseToken_Success() {
+        // Given
+        String email = "test@example.com";
+        String token = jwtUtil.generateAccessToken(email);
+
+        // When
+        Claims claims = jwtUtil.parseToken(token);
+
+        // Then
+        assertEquals(email, claims.getSubject());
+    }
+
+    @Test
+    void parseToken_InvalidToken_ThrowsException() {
+        // Given: 유효하지 않은 토큰
+        String invalidToken = "invalidToken";
+
+        // When & Then: 예외가 발생하는지 확인
+        CustomException exception = assertThrows(CustomException.class, () -> jwtUtil.parseToken(invalidToken));
+        assertEquals(ErrorMsg.INVALID_TOKEN.getCode(), exception.getCode());
+    }
+
+    @Test
     void getEmailFromToken_Success() {
         // Given
         String email = "test@example.com";
@@ -80,36 +134,12 @@ class JwtUtilTest {
 
     @Test
     void getEmailFromToken_InvalidToken_ThrowsException() {
-        // Given
+        // Given: 유효하지 않은 토큰
         String invalidToken = "invalidToken";
 
-        // When & Then
+        // When & Then: 예외가 발생하는지 확인
         CustomException exception = assertThrows(CustomException.class, () -> jwtUtil.getEmailFromToken(invalidToken));
         assertEquals(ErrorMsg.INVALID_TOKEN.getCode(), exception.getCode());
-    }
-
-    @Test
-    void validateToken_ValidToken_ReturnsTrue() {
-        // Given
-        String token = jwtUtil.generateAccessToken("test@example.com");
-
-        // When
-        boolean isValid = jwtUtil.validateToken(token);
-
-        // Then
-        assertTrue(isValid);
-    }
-
-    @Test
-    void validateToken_InvalidToken_ReturnsFalse() {
-        // Given
-        String invalidToken = "invalidToken";
-
-        // When
-        boolean isValid = jwtUtil.validateToken(invalidToken);
-
-        // Then
-        assertFalse(isValid);
     }
 
     @Test
@@ -143,11 +173,33 @@ class JwtUtilTest {
         // Given: 유효하지 않은 토큰
         String invalidToken = "invalidToken";
 
-        // When & Then: extractEmail 메서드를 호출했을 때 CustomException이 발생하는지 확인
-        CustomException exception = assertThrows(CustomException.class, () -> jwtUtil.extractEmail(invalidToken));
-        assertEquals(ErrorMsg.INVALID_TOKEN.getCode(), exception.getCode());
+        // When & Then: 예외가 발생하는지 확인
+        assertThrows(MalformedJwtException.class, () -> jwtUtil.extractEmail(invalidToken));
     }
 
+    @Test
+    void validateToken_ValidToken_ReturnsTrue() {
+        // Given
+        String token = jwtUtil.generateAccessToken("test@example.com");
+
+        // When
+        boolean isValid = jwtUtil.validateToken(token);
+
+        // Then
+        assertTrue(isValid);
+    }
+
+    @Test
+    void validateToken_InvalidToken_ReturnsFalse() {
+        // Given: 유효하지 않은 토큰
+        String invalidToken = "invalidToken";
+
+        // When
+        boolean isValid = jwtUtil.validateToken(invalidToken);
+
+        // Then
+        assertFalse(isValid);
+    }
 
     @Test
     void revokeToken_ValidToken_Success() {
@@ -177,103 +229,4 @@ class JwtUtilTest {
         // Then
         verify(refreshTokenRepository, never()).save(any());
     }
-
-//    @Test
-//    void refreshToken_Success() throws Exception {
-//        // Given
-//        HttpServletRequest request = mock(HttpServletRequest.class);
-//        HttpServletResponse response = mock(HttpServletResponse.class);
-//        String refreshToken = jwtUtil.generateRefreshToken("test@example.com");
-//        when(request.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn("Bearer " + refreshToken);
-//        when(membersRepository.findByEmail(anyString())).thenReturn(Optional.of(mock(Members.class)));
-//
-//        // When
-//        jwtUtil.refreshToken(request, response);
-//
-//        // Then
-//        verify(response).setHeader(eq("Authorization"), anyString());
-//    }
-
-    @Test
-    void refreshToken_MissingAuthorizationHeader_UnauthorizedResponse() throws Exception {
-        // Given: Authorization 헤더가 없을 경우
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        when(request.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn(null);
-
-        // When: refreshToken 메서드를 호출
-        jwtUtil.refreshToken(request, response);
-
-        // Then: Unauthorized 상태 코드가 설정되었는지 확인
-        verify(response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        verify(response, never()).setHeader(anyString(), anyString());
-    }
-
-
-    @Test
-    void refreshToken_InvalidToken_UnauthorizedResponse() throws Exception {
-        // Given
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        String invalidToken = "invalidToken";
-        when(request.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn("Bearer " + invalidToken);
-
-        // When & Then: refreshToken 메서드를 호출했을 때 CustomException이 발생하는지 확인
-        CustomException exception = assertThrows(CustomException.class, () -> {
-            jwtUtil.refreshToken(request, response);
-        });
-
-        // CustomException의 메시지나 코드가 예상대로 반환되는지 확인
-        assertEquals(ErrorMsg.INVALID_TOKEN.getCode(), exception.getCode());
-    }
-
-    @Test
-    void refreshToken_InvalidBearerScheme_UnauthorizedResponse() throws Exception {
-        // Given: Authorization 헤더가 Bearer로 시작하지 않는 경우
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        when(request.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn("InvalidScheme refreshToken");
-
-        // When: refreshToken 메서드를 호출
-        jwtUtil.refreshToken(request, response);
-
-        // Then: Unauthorized 상태 코드가 설정되었는지 확인
-        verify(response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        verify(response, never()).setHeader(anyString(), anyString());
-    }
-
-    @Test
-    void refreshToken_NonExistentUser_UnauthorizedResponse() throws Exception {
-        // Given: 토큰에 해당하는 사용자가 존재하지 않을 경우
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        String refreshToken = jwtUtil.generateRefreshToken("test@example.com");
-        when(request.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn("Bearer " + refreshToken);
-        when(membersRepository.findByEmail(anyString())).thenReturn(Optional.empty());
-
-        // When: refreshToken 메서드를 호출
-        jwtUtil.refreshToken(request, response);
-
-        // Then: Unauthorized 상태 코드가 설정되었는지 확인
-        verify(response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        verify(response, never()).setHeader(anyString(), anyString());
-    }
-
-    @Test
-    void refreshToken_ValidToken_UserNotFound_UnauthorizedResponse() throws Exception {
-        // Given: 유효한 토큰이지만 사용자가 없는 경우
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
-        String refreshToken = jwtUtil.generateRefreshToken("test@example.com");
-        when(request.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn("Bearer " + refreshToken);
-        when(membersRepository.findByEmail(anyString())).thenReturn(Optional.empty());
-
-        // When: refreshToken 메서드를 호출
-        jwtUtil.refreshToken(request, response);
-
-        // Then: Unauthorized 상태 코드가 설정되었는지 확인
-        verify(response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        verify(response, never()).setHeader(anyString(), anyString());
-    }
 }
-
