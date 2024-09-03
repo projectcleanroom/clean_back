adfc2eabd79d4b484568a3bb3036364fcd8e4548
HyeonJeongJae <guswjdwo3427@naver.com>
2024-09-03 16:15:18 +0900

    Update MembersLoginAndLogoutController.java

--- src/main/java/com/clean/cleanroom/members/controller/MembersLoginAndLogoutController.java
+++ src/main/java/com/clean/cleanroom/members/controller/MembersLoginAndLogoutController.java
@@ -33,7 +33,7 @@ public class MembersLoginAndLogoutController {
         return membersService.logout(accessToken, refreshToken);
     }
 
-    @PostMapping("/kakao-login")
+    @GetMapping("/kakao-login")
     public void socialKakaoLogin(@RequestBody KakaoAuthCodeRequestDto kakaoAuthCodeRequestDto, HttpServletResponse response) throws IOException {
         kakaoLoginService.socialKakaoLogin(kakaoAuthCodeRequestDto, response);
     }
