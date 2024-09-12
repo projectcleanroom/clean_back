63371946c38fffebf02a9ec937088eeba98ce6a2
sion <topaegr3525@kakao.com>
2024-09-12 23:31:43 +0900

    feat : redis 설정추가

--- src/main/java/com/clean/cleanroom/config/RedisConfig.java
+++ src/main/java/com/clean/cleanroom/config/RedisConfig.java
@@ -1,9 +1,13 @@
 package com.clean.cleanroom.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -16,15 +20,27 @@ public class RedisConfig {
         // Redis 연결 팩토리 설정
         template.setConnectionFactory(redisConnectionFactory);
 
-        // 키는 String으로, 값은 기본적으로 직렬화된 객체로 설정
+        // ObjectMapper 설정
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());  // Java 8 Date/Time 모듈 추가
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);  // ISO-8601 포맷으로 날짜 처리
+
+        // GenericJackson2JsonRedisSerializer에 ObjectMapper 적용
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+        // 키는 String으로 직렬화
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
 
-        // 옵션: Hash의 키와 값에 대한 직렬화 설정
+        // 값은 JSON으로 직렬화하여 객체를 저장 가능하게 설정
+        template.setValueSerializer(serializer);
+
+        // Hash의 키와 값에 대한 직렬화 설정
         template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
 
-        template.setEnableTransactionSupport(true); // 트랜잭션 지원 여부
+        // 트랜잭션 지원 여부 설정
+        template.setEnableTransactionSupport(true);
+
         template.afterPropertiesSet();
         return template;
     }
